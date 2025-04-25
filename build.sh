355d9899c9016b6b517d498a1a4d8d9cdcd1b764
Henk <henk@henk.tech>
2025-04-25 17:58:56 +0200

    Better build script

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-sudo ./d2vm build . -o uftc.vhd --bootloader grub --boot-size 4000 --size 14G
+sudo docker build . -t uftc && sudo rm uftc.vhd
+sudo ./d2vm convert uftc:latest -o uftc.vhd --bootloader grub --boot-size 4000 --size 14G $*
